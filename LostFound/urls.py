c1c4fb6eb3b7247fdfa5b7a394961e6e0e621cc8
faizinahsan <faizin110998@gmail.com>
2018-11-06 19:52:18 +0700

    belajar registrasi

--- LostFound/urls.py
+++ LostFound/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from users import views as user_views
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('register/', user_views.register,name='register'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
