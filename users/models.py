9fd484b773a054d9ea5567736307c322a1b9b54c
faizinahsan <faizin110998@gmail.com>
2018-11-07 08:17:10 +0700

    Auth 1.0

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# class Registration(models.Model):
+#     username = models.CharField(max_length=50)
+#     email = models.EmailField(max_length=254)
