f383d827b3469471b91e651ff38e25c2d82ad7b8
faizinahsan <faizin110998@gmail.com>
2018-11-07 08:31:20 +0700

    Auth 1.0

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
 class Post(models.Model):
 	"""docstring for ClassName"""
