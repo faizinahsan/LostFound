18d204a40bbd3b13385535bff5f6e49d12fa1371
faizinahsan <faizin110998@gmail.com>
2018-11-07 10:39:07 +0700

    Auth diperbaiki

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-# from users.forms import RegistrationForm
+from users.forms import RegistrationForm
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
+    form = RegistrationForm()   
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)   
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -13,6 +14,6 @@ def register(request):
             messages.success(request,f'Account created for {username}!')
             messages.success(request,f'Account created for {email}!')
             return redirect('blog-home')
-    else:
-        form = UserCreationForm()
+        else:
+            form = RegistrationForm()
     return render(request,'users/masuk.html',{'form':form})
