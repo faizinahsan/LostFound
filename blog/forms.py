ef44628360243d7851d7d89fc3ec939ab957d997
Satriosadrakha <mc.mealant@gmail.com>
2018-12-04 12:57:52 +0700

    MAPS

--- blog/forms.py
+++ blog/forms.py
@@ -4,8 +4,10 @@ from .models import Post,Log, MyModel
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'body','image','idCategories','idTypes')
+        fields = ('title', 'body','image','idCategories','idTypes','latitude','longitude')
+        # print (fields[5])
         # fields = ('title', 'body','area','image','latitude','longitude','state','idCategories','idTypes')
+
 class PostLogForm(forms.ModelForm):
     class Meta:
         model = Log
